d095344a6c1fc20ca63c4915b78c100a5e9e13bb
Suryansh Chaurasia <csuryansh23@gmail.com>
2022-09-23 11:18:18 +0530

    added submodule utility

---
+++ src/main/java/com/example/demo/UtilityCheck.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import com.example.utility.*;
+
+public class UtilityCheck {
+
+    public static void printMessages() {
+        Util.sayHello();
+
+        Util util = new Util();
+        util.sayGoodBye();
+    }
+
+}
